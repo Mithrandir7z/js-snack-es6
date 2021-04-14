06ee18249fa7e247e30783c49b11cb545c493adf
MattiaManfreda <mattiamanfreda926@gmail.com>
2021-04-14 14:19:16 +0200

    agg

--- js/script.js
+++ js/script.js
@@ -9,4 +9,48 @@
 
 
 
-// Ex 1
+// Ex 1 
+console.log("Ex 1");
+
+//Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+let bici = [
+    {
+        'nome': 'Mountain Bike',
+        'peso': 12
+    } ,
+    {
+        'nome': 'Bmx',
+        'peso': 8
+    } ,
+    {
+        'nome': 'Graziella',
+        'peso': 17
+    } ,
+    {
+        'nome': 'Ducati',
+        'peso': 4
+    } ,
+    {
+        'nome': 'Alpine',
+        'peso': 5
+    }
+];
+
+console.log(bici);
+
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal Snack
+var pesoMinore = bici[0].peso;
+console.log(pesoMinore);
+
+// --> Leggere intanto i dati con un for e determinare la bici col peso minore
+for ( let i = 0; i < bici.length; i++ ) {
+    let thisObject = bici[i];
+
+    if (thisObject.peso < pesoMinore) {
+        pesoMinore = thisObject.peso;
+    }
+}
+
+console.log("La bicicletta più leggera è di: ", pesoMinore, "kg, ed è la: ");
+
+// Dopo di che destrutturare quell'array con peso minore e stamparlo in un ul li col template literal.
